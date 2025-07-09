793a989601afbb529db975e60bc6492cd1bb0b6b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-10 04:22:55 +0500

    add controller

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final List<Map<String, dynamic>> socialPlatforms = [
+    {
+      'name': 'Facebook',
+      'url': 'https://facebook.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png',
+    },
+    {
+      'name': 'Pinterest',
+      'url': 'https://pinterest.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png',
+    },
+    {
+      'name': 'Twitter',
+      'url': 'https://twitter.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/en/6/60/Twitter_Logo_as_of_2021.svg',
+    },
+    {
+      'name': 'Snapchat',
+      'url': 'https://snapchat.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/en/a/ad/Snapchat_logo.svg',
+    },
+    {
+      'name': 'Instagram',
+      'url': 'https://instagram.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
+    },
+    {
+      'name': 'TikTok',
+      'url': 'https://tiktok.com',
+      'icon': 'https://upload.wikimedia.org/wikipedia/en/0/0a/TikTok_logo.svg',
+    },
+    {
+      'name': 'YouTube',
+      'url': 'https://youtube.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg',
+    },
+    {
+      'name': 'Reddit',
+      'url': 'https://reddit.com',
+      'icon':
+          'https://upload.wikimedia.org/wikipedia/en/5/58/Reddit_logo_new.svg',
+    },
+  ];
+}
