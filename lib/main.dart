9760678e3e460f088a46ab010dadf50f96f21154
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-10 04:22:39 +0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:sizer/sizer.dart';
+
+import 'package:stay_connected/routes/app_pages.dart';
+
+void main() async {
+//  Get.lazyPut<SocketService>(() => SocketService(), fenix: true);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return Sizer(
+      builder: (context, orientation, deviceType) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          // theme: ThemeLight().theme,
+          builder: EasyLoading.init(),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        ),
       ),
-      home:Container()
     );
   }
 }
