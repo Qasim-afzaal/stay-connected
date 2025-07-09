26ac523b0f81c18d7cb4a84c6d83fa94c05e01a4
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-10 04:22:49 +0500

    add home binding

---
+++ lib/home/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:stay_connected/home/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
