7fffa1f1bf0eb29f0abe3ffaa335612359f9afd0
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-14 03:05:53 +0500

    add instagram binding

---
+++ lib/Platform/instagram/instagram_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:stay_connected/Platform/instagram/instagram_controller.dart';
+
+class InstagramBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<InstagramController>(
+      () => InstagramController(""),
+    );
+  }
+} 
