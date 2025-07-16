3bbb92bdf5bd636bc43ed94cea890d9b960b2d0a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-16 18:51:18 +0500

    add snapchat binding

---
+++ lib/Platform/snapchat/snapchat_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:stay_connected/Platform/snapchat/snapchat_controller.dart';
+
+class SnapchatBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<SnapchatController>(
+      () => SnapchatController(""),
+    );
+  }
+} 
