ae3ce48a69f35262bfe68744470f93b78ca203c1
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-17 03:36:40 +0500

    add tiktok binding

---
+++ lib/Platform/tiktok/tiktok_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:stay_connected/Platform/tiktok/tiktok_controller.dart';
+
+class TikTokBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<TikTokController>(
+      () => TikTokController(""),
+    );
+  }
+} 
