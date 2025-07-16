425a496fdbc083b1d923142daa83ec645b1f2525
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-17 03:36:54 +0500

    load tiktok defualt icon

---
+++ lib/Platform/tiktok/tiktok_controller.dart
@@ -0,0 +1,86 @@
+// tiktok_controller.dart
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TikTokController extends GetxController {
+  final String platformName;
+  List<Map<String, String>> icons = [];
+  String selectedPlatformName = '';
+  static const String _sharedPrefsKey = 'platform_icons_data';
+
+  final List<Map<String, String>> sharedCategories = [
+    {
+      'title': 'Favorite',
+      'icon': 'assets/icons/home.svg'
+    },
+    {
+      'title': 'Game',
+      'icon': 'assets/icons/chat.svg'
+    },
+    {
+      'title': 'Entertainment',
+      'icon': 'assets/icons/settings.svg'
+    },
+    {
+      'title': 'Work',
+      'icon': 'assets/icons/account_circle.svg'
+    },
+  ];
+
+  TikTokController(this.platformName);
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    loadIcons();
+  }
+
+  Future<void> loadIcons() async {
+    final prefs = await SharedPreferences.getInstance();
+    final allData = prefs.getString(_sharedPrefsKey);
+
+    if (allData != null) {
+      final decoded = jsonDecode(allData) as Map<String, dynamic>;
+      final platformData = decoded[platformName];
+
+      if (platformData != null) {
+        icons = List<Map<String, String>>.from(platformData);
+      } else {
+        icons = [];
+      }
+    } else {
+      icons = [];
+    }
+    update();
+  }
+
+  Future<void> addIcon(String name, String iconUrl) async {
+    icons.add({'name': name, 'icon': iconUrl});
+    await _saveToPrefs();
+    update();
+  }
+
+  Future<void> removeIcon(int index) async {
+    if (index >= 0 && index < icons.length) {
+      icons.removeAt(index);
+      await _saveToPrefs();
+      update();
+    }
+  }
+
+  Future<void> _saveToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final allData = prefs.getString(_sharedPrefsKey);
+    Map<String, dynamic> dataMap = {};
+
+    if (allData != null) {
+      dataMap = Map<String, dynamic>.from(jsonDecode(allData));
+    }
+
+    dataMap[platformName] = icons;
+    await prefs.setString(_sharedPrefsKey, jsonEncode(dataMap));
+  }
+} 
