212a69ccb61f5b4f5d00d004e3f9482479805638
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-18 04:59:26 +0500

    initiaze routes

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,6 +2,20 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 
 import 'package:stay_connected/Platform/facebook/facebook_binding.dart';
 import 'package:stay_connected/Platform/facebook/facebook_page.dart';
+import 'package:stay_connected/Platform/instagram/instagram_binding.dart';
+import 'package:stay_connected/Platform/instagram/instagram_page.dart';
+import 'package:stay_connected/Platform/youtube/youtube_binding.dart';
+import 'package:stay_connected/Platform/youtube/youtube_page.dart';
+import 'package:stay_connected/Platform/twitter/twitter_binding.dart';
+import 'package:stay_connected/Platform/twitter/twitter_page.dart';
+import 'package:stay_connected/Platform/tiktok/tiktok_binding.dart';
+import 'package:stay_connected/Platform/tiktok/tiktok_page.dart';
+import 'package:stay_connected/Platform/reddit/reddit_binding.dart';
+import 'package:stay_connected/Platform/reddit/reddit_page.dart';
+import 'package:stay_connected/Platform/snapchat/snapchat_binding.dart';
+import 'package:stay_connected/Platform/snapchat/snapchat_page.dart';
+import 'package:stay_connected/Platform/pinterest/pinterest_binding.dart';
+import 'package:stay_connected/Platform/pinterest/pinterest_page.dart';
 import 'package:stay_connected/home/home.dart';
 import 'package:stay_connected/home/home_binding.dart';
 import 'package:stay_connected/splash/splash.dart';
@@ -30,5 +44,33 @@ class AppPages {
         name: _Paths.FACEBOOK,
         page: () => FacebookPage(),
         binding: FacebookBinding()),
+    GetPage(
+        name: _Paths.INSTAGRAM,
+        page: () => InstagramPage(),
+        binding: InstagramBinding()),
+    GetPage(
+        name: _Paths.YOUTUBE,
+        page: () => YouTubePage(),
+        binding: YouTubeBinding()),
+    GetPage(
+        name: _Paths.TWITTER,
+        page: () => TwitterPage(),
+        binding: TwitterBinding()),
+    GetPage(
+        name: _Paths.TIKTOK,
+        page: () => TikTokPage(),
+        binding: TikTokBinding()),
+    GetPage(
+        name: _Paths.REDDIT,
+        page: () => RedditPage(),
+        binding: RedditBinding()),
+    GetPage(
+        name: _Paths.SNAPCHAT,
+        page: () => SnapchatPage(),
+        binding: SnapchatBinding()),
+    GetPage(
+        name: _Paths.PINTEREST,
+        page: () => PinterestPage(),
+        binding: PinterestBinding()),
   ];
 }
