ab848360844a35e62e2f408e00dd098a62e6f455
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-19 05:27:16 +0500

    create routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,33 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const SPLASH = _Paths.SPLASH;
+  static const WEBVIEW = _Paths.WEBVIEW;
+  static const HOME = _Paths.HOME;
+
+  static const FACEBOOK = _Paths.FACEBOOK;
+  static const INSTAGRAM = _Paths.INSTAGRAM;
+  static const YOUTUBE = _Paths.YOUTUBE;
+  static const TWITTER = _Paths.TWITTER;
+  static const TIKTOK = _Paths.TIKTOK;
+  static const REDDIT = _Paths.REDDIT;
+  static const SNAPCHAT = _Paths.SNAPCHAT;
+  static const PINTEREST = _Paths.PINTEREST;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const SPLASH = '/splash';
+  static const WEBVIEW = '/web_view';
+  static const HOME = '/home';
+  static const FACEBOOK = '/facebook';
+  static const INSTAGRAM = '/instagram';
+  static const YOUTUBE = '/youtube';
+  static const TWITTER = '/twitter';
+  static const TIKTOK = '/tiktok';
+  static const REDDIT = '/reddit';
+  static const SNAPCHAT = '/snapchat';
+  static const PINTEREST = '/pinterest';
+}
