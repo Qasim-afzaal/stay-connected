670d5a13d5c38c06f569244184b5b72e7dd4e5d6
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-21 05:07:34 +0500

    add app strings

---
+++ lib/util/util.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HttpUtil {
+  // Body params name ->
+  static String name = "name";
+  static String firstName = "firstname";
+  static String lastName = "lastname";
+  static String email = "email";
+  static String password = "password";
+  static String authProvider = "auth_provider";
+  static String gender = "gender";
+  static String age = "age";
+  static String personalityType = "personality_type";
+  static String profileImageUrl = "profile_image_url";
+  static String otp = "otp";
+  static String pin = "pin";
+  static String newPin = "newPin";
+  static String oldPin = "oldPin";
+  static String deviceId = "device_id";
+  static String isForgetPin = "isForgetPin";
+  static String conversationId = "conversation_id";
+  static String isArchive = "isArchive";
+  static String message = "message";
+  static String objective = "objective";
+  static String selectedGift = "selected_gift";
+  static String selectedRelation = "selected_relation";
+  static String selectedRelationType = "selected_relation_type";
+  static String selectedRelationVibe = "selected_relation_vibe";
+  static String selectedRelationRange = "selected_relation_range";
+  static String imageAnalyzer = "image_analyzer";
+  static String dateIdeaGen = "date_idea";
+
+  static String content = "content";
+  static String userId = "user_id";
+  static String id = "id";
+  static String fileData = "file_data";
+  static String skip = "skip";
+  static String size = "size";
+  static String filePath = "filePath";
+  static String messageId = "message_id";
+  static String isManually = "is_manually";
+  static String isSparkDLine = "is_sparkd_line";
+  static const String previousSuggestions = "previous_suggestions";
+  static const String giftFor = "gift_for";
+  static const String partnershipStage = "partnership_stage";
+  static const String recipientInterest = "recipient_interest";
+  static const String giverVibe = "giver_vibe";
+  static const String budget = "budget";
+  static const String nameForGift = "name";
+  static const String genderOfGift = "gender";
+  static const String ageOfGift = "age";
+  static const String personality = "personality";
+}
