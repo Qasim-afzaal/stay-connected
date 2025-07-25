833f5a26bb1164b0d13cb04d5b515411ca761c45
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-07-26 03:24:39 +0500

    add home screen

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get/get.dart';
+import 'package:get/get.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:stay_connected/home/home_controller.dart';
 import 'package:stay_connected/routes/app_pages.dart';
@@ -8,6 +17,10 @@ import 'package:stay_connected/util/util.dart';
 import 'package:stay_connected/webview/webview_binding.dart';
 import 'package:stay_connected/webview/webview_page.dart';
 import 'package:stay_connected/widget/custom_drwaer.dart';
+import 'package:stay_connected/widget/custom_drwaer.dart';
+import 'package:stay_connected/widget/custom_drwaer.dart';
+import 'package:stay_connected/widget/icon_selector.dart';
+import 'package:stay_connected/widget/icon_selector.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,56 +29,101 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(
       init: HomeController(),
-      builder: (controller) {
+      builder: (HomeController controller) {
         return Scaffold(
           drawer: const CustomDrawer(),
           appBar: AppBar(
             title: const Text('Stay Connected'),
             centerTitle: true,
-            backgroundColor: Colors.blue,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(15),
-            child: GridView.builder(
-              itemCount: controller.socialPlatforms.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 20,
-                childAspectRatio: 2,
+          body: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.asset(
+                  'assets/images/img_group_173.jpg',
+                  fit: BoxFit.cover,
+                ),
               ),
-              itemBuilder: (context, index) {
-                final item = controller.socialPlatforms[index];
-                return GestureDetector(
-                  onTap: () {
-                    print(
-                      item['name'],
-                    );
-                    Get.toNamed(Routes.FACEBOOK, arguments: {
-                      HttpUtil.name: item['name'],
-                    });
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(16),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Image.network(
-                        item['icon'],
-                        height: 40,
-                        width: 40,
-                        fit: BoxFit.contain,
-                        errorBuilder: (_, __, ___) => const Icon(Icons.link),
-                      ),
-                    ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: GridView.builder(
+                  itemCount: controller.socialPlatforms.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20,
+                    childAspectRatio: 2,
                   ),
-                );
-              },
-            ),
+                  itemBuilder: (context, index) {
+                    final item = controller.socialPlatforms[index];
+                    return GestureDetector(
+                      onTap: () {
+                        print(item['name']);
+                        switch (item["name"].toString().toLowerCase()) {
+                          case "facebook":
+                            Get.offNamed(Routes.FACEBOOK);
+                            break;
+                          case "instagram":
+                            Get.offNamed(Routes.INSTAGRAM);
+                            break;
+                          case "youtube":
+                            Get.offNamed(Routes.YOUTUBE);
+                            break;
+                          case "twitter":
+                            Get.offNamed(Routes.TWITTER);
+                            break;
+                          case "tiktok":
+                            Get.offNamed(Routes.TIKTOK);
+                            break;
+                          case "reddit":
+                            Get.offNamed(Routes.REDDIT);
+                            break;
+                          case "snapchat":
+                            Get.offNamed(Routes.SNAPCHAT);
+                            break;
+                          case "pinterest":
+                            Get.offNamed(Routes.PINTEREST);
+                            break;
+                          case "tumblr":
+                            // Add navigation if needed
+                            break;
+                          default:
+                            Get.offNamed(Routes.FACEBOOK);
+                            break;
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(16),
+                        child: Container(
+                          width: 90,
+                          height: 90,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            shape: BoxShape.circle,
+                          ),
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            item['icon'],
+                            height: 70,
+                            width: 70,
+                            fit: BoxFit.contain,
+                            errorBuilder: (_, __, ___) =>
+                                const Icon(Icons.link),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         );
       },
