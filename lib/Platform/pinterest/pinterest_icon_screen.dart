2560c9f3b0047ba82f1b4eb685d0f0860285147f
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-12-18 04:05:17 +0500

    feat: Use iconnew_nbg.png with 30x30 size in dark mode for Pinterest icon screen

--- lib/Platform/pinterest/pinterest_icon_screen.dart
+++ lib/Platform/pinterest/pinterest_icon_screen.dart
@@ -101,12 +101,15 @@ class PinterestIconScreen extends StatelessWidget {
                               Container(
                                 padding: const EdgeInsets.all(4),
                                 child: Image.asset(
-                                  'assets/images/img_pintrest.png',
-                                  scale: 0.1,
+                                  isDark ? 'assets/images/iconnew_nbg.png' : 'assets/images/img_pintrest.png',
+                                  width: isDark ? 30 : null,
+                                  height: isDark ? 30 : null,
+                                  scale: isDark ? null : 0.1,
+                                  fit: BoxFit.contain,
                                   errorBuilder: (context, error, stackTrace) {
                                     return Icon(
                                       Icons.person,
-                                      size: 50,
+                                      size: isDark ? 30 : 50,
                                       color: Colors.blue.shade700,
                                     );
                                   },
