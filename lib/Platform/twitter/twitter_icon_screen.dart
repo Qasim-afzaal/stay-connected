128996b31b6e6e135aee908609052aaed8bbbfbe
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-12-18 04:05:14 +0500

    feat: Use iconnew_nbg.png with 30x30 size in dark mode for Twitter icon screen

--- lib/Platform/twitter/twitter_icon_screen.dart
+++ lib/Platform/twitter/twitter_icon_screen.dart
@@ -109,12 +109,15 @@ class TwitterIconScreen extends StatelessWidget {
                               Container(
                                 padding: const EdgeInsets.all(4),
                                 child: Image.asset(
-                                  'assets/images/img_twitter.png',
-                                  scale: 0.1,
+                                  isDark ? 'assets/images/iconnew_nbg.png' : 'assets/images/img_twitter.png',
+                                  width: isDark ? 30 : null,
+                                  height: isDark ? 30 : null,
+                                  scale: isDark ? null : 0.1,
+                                  fit: BoxFit.contain,
                                   errorBuilder: (context, error, stackTrace) {
                                     return Icon(
                                       Icons.person,
-                                      size: 50,
+                                      size: isDark ? 30 : 50,
                                       color: Colors.blue.shade700,
                                     );
                                   },
