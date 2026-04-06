47da1d67e69ee330d9c2d19a5a07afead0a15357
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-04-07 02:56:34 +0500

    feat(facebook): integrate FilteredFriendsLayer for improved friend display

--- lib/Platform/facebook/facebook_icon_screen.dart
+++ lib/Platform/facebook/facebook_icon_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:stay_connected/Platform/facebook/facebook_controller.dart';
 import 'package:stay_connected/Platform/facebook/facebook_webview_screen.dart';
 import 'package:stay_connected/Platform/facebook/facebook_constants.dart';
+import 'package:stay_connected/widget/filtered_friends_layer.dart';
 
 class FacebookIconScreen extends StatelessWidget {
   final String iconName;
@@ -62,95 +63,110 @@ class FacebookIconScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            child: categoryFriends.isEmpty
-                ? Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.people_outline,
-                          size: 64,
-                          color: isDark ? Colors.grey[600] : Colors.grey.shade400,
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          FacebookConstants.emptyCategoryMessage,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: isDark ? Colors.grey[400] : Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
+            child: FilteredFriendsLayer(
+              isDark: isDark,
+              friends: categoryFriends,
+              emptyChild: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.people_outline,
+                      size: 64,
+                      color: isDark ? Colors.grey[600] : Colors.grey.shade400,
                     ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: GridView.builder(
-                      itemCount: categoryFriends.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 16,
-                        crossAxisSpacing: 16,
-                        childAspectRatio: 0.8,
+                    const SizedBox(height: 16),
+                    Text(
+                      FacebookConstants.emptyCategoryMessage,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: isDark ? Colors.grey[400] : Colors.grey.shade500,
                       ),
-                      itemBuilder: (context, index) {
-                        final friend = categoryFriends[index];
-                        return GestureDetector(
-                          onTap: () {
-                            if (friend['profileUrl'] != null) {
-                              Get.to(() => _FriendProfileWebView(
-                                    profileUrl: friend['profileUrl']!,
-                                    friendName: friend['name'] ?? FacebookConstants.unknownFriend,
-                                  ));
-                            }
-                          },
-                          onLongPress: () {
-                            _showActionDialog(
-                                context, friend['name'] ?? FacebookConstants.unknownFriend, index);
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(4),
-                                child: Image.asset(
-                                  isDark ? FacebookConstants.assetAccountFb : FacebookConstants.assetImgFb,
-                                  width: isDark ? 60 : null,
-                                  height: isDark ? 60 : null,
-                                  scale: isDark ? null : 0.1,
-                                  fit: BoxFit.contain,
-                                  errorBuilder: (context, error, stackTrace) {
-                                    return Icon(
-                                      Icons.person,
-                                      size: isDark ? 30 : 50,
-                                      color: Colors.blue.shade700,
-                                    );
-                                  },
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 4),
-                                child: Text(
-                                  friend['name'] ?? FacebookConstants.unknownFriend,
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                    color: isDark ? Colors.grey[300] : Colors.black,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
                     ),
+                  ],
+                ),
+              ),
+              contentBuilder: (context, filtered, all) {
+                return Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: GridView.builder(
+                    itemCount: filtered.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      childAspectRatio: 0.72,
+                    ),
+                    itemBuilder: (context, index) {
+                      final friend = filtered[index];
+                      final fullIndex = all.indexWhere(
+                        (e) =>
+                            (e['profileUrl'] ?? '') ==
+                            (friend['profileUrl'] ?? ''),
+                      );
+                      return GestureDetector(
+                        onTap: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          if (friend['profileUrl'] != null) {
+                            Get.to(() => _FriendProfileWebView(
+                                  profileUrl: friend['profileUrl']!,
+                                  friendName: friend['name'] ??
+                                      FacebookConstants.unknownFriend,
+                                ));
+                          }
+                        },
+                        onLongPress: () {
+                          _showActionDialog(
+                            context,
+                            friend['name'] ?? FacebookConstants.unknownFriend,
+                            fullIndex >= 0 ? fullIndex : 0,
+                          );
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(4),
+                              child: Image.asset(
+                                isDark ? FacebookConstants.assetAccountFb : FacebookConstants.assetImgFb,
+                                width: isDark ? 60 : null,
+                                height: isDark ? 60 : null,
+                                scale: isDark ? null : 0.1,
+                                fit: BoxFit.contain,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Icon(
+                                    Icons.person,
+                                    size: isDark ? 30 : 50,
+                                    color: Colors.blue.shade700,
+                                  );
+                                },
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 4),
+                              child: Text(
+                                friend['name'] ?? FacebookConstants.unknownFriend,
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  color: isDark ? Colors.grey[300] : Colors.black,
+                                ),
+                                textAlign: TextAlign.center,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
+                );
+              },
+            ),
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () => _showSearchDialog(context),
