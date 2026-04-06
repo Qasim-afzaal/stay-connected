8ab83b6b0b01dd16268420f2acf5d4fb559c65cf
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-04-07 02:57:13 +0500

    feat(twitter): integrate FilteredFriendsLayer for enhanced friend display and interaction

--- lib/Platform/twitter/twitter_icon_screen.dart
+++ lib/Platform/twitter/twitter_icon_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:stay_connected/Platform/twitter/twitter_controller.dart';
 import 'package:stay_connected/Platform/twitter/twitter_search_dialog.dart';
 import 'package:stay_connected/Platform/twitter/twitter_constants.dart';
+import 'package:stay_connected/widget/filtered_friends_layer.dart';
 
 class TwitterIconScreen extends StatelessWidget {
   final String iconName;
@@ -62,54 +63,67 @@ class TwitterIconScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            child: categoryFriends.isEmpty
-                ? Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.people_outline,
-                          size: 64,
-                          color: isDark ? Colors.grey[600] : Colors.grey.shade400,
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          TwitterConstants.emptyCategoryMessage,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: isDark ? Colors.grey[400] : Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
+            child: FilteredFriendsLayer(
+              isDark: isDark,
+              friends: categoryFriends,
+              emptyChild: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.people_outline,
+                      size: 64,
+                      color: isDark ? Colors.grey[600] : Colors.grey.shade400,
                     ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: GridView.builder(
-                      itemCount: categoryFriends.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 16,
-                        crossAxisSpacing: 16,
-                        childAspectRatio: 0.8,
+                    const SizedBox(height: 16),
+                    Text(
+                      TwitterConstants.emptyCategoryMessage,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: isDark ? Colors.grey[400] : Colors.grey.shade500,
                       ),
-                      itemBuilder: (context, index) {
-                        final friend = categoryFriends[index];
-                        return GestureDetector(
-                          onTap: () {
-                            if (friend['profileUrl'] != null) {
-                              Get.to(() => _FriendProfileWebView(
-                                    profileUrl: friend['profileUrl']!,
-                                    friendName: friend['name'] ?? TwitterConstants.unknownFriend,
-                                  ));
-                            }
-                          },
-                          onLongPress: () {
-                            _showActionDialog(
-                                context, friend['name'] ?? 'Unknown', index);
-                          },
-                          child: Column(
+                    ),
+                  ],
+                ),
+              ),
+              contentBuilder: (context, filtered, all) {
+                return Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: GridView.builder(
+                    itemCount: filtered.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      childAspectRatio: 0.72,
+                    ),
+                    itemBuilder: (context, index) {
+                      final friend = filtered[index];
+                      final fullIndex = all.indexWhere(
+                        (e) =>
+                            (e['profileUrl'] ?? '') ==
+                            (friend['profileUrl'] ?? ''),
+                      );
+                      return GestureDetector(
+                        onTap: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          if (friend['profileUrl'] != null) {
+                            Get.to(() => _FriendProfileWebView(
+                                  profileUrl: friend['profileUrl']!,
+                                  friendName: friend['name'] ??
+                                      TwitterConstants.unknownFriend,
+                                ));
+                          }
+                        },
+                        onLongPress: () {
+                          _showActionDialog(
+                            context,
+                            friend['name'] ?? 'Unknown',
+                            fullIndex >= 0 ? fullIndex : 0,
+                          );
+                        },
+                        child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Container(
@@ -150,7 +164,9 @@ class TwitterIconScreen extends StatelessWidget {
                         );
                       },
                     ),
-                  ),
+                  );
+              },
+            ),
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () => _showSearchDialog(context),
